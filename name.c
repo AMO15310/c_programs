abead2e0ffe4c36c68d5a3df0d7e24b781c9ac6b
AMO15310 <wachiraamos402@gmail.com>
2022-03-26 20:53:16 -0500

    write your name

---
+++ name.c
@@ -0,0 +1,17 @@
+// program to write your name
+#include <stdio.h>
+
+int main ()
+{
+char name[10];
+printf("please enter your name: ");
+scanf("%s",name);
+printf("hello %s",name);
+    
+    
+    
+
+
+    
+    return 0; 
+}
