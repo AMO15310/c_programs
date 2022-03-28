71ac703b20dc4959c0128e542702f879f1209de6
AMO15310 <wachiraamos402@gmail.com>
2022-03-28 18:53:08 -0500

    find the sum of two numbers

---
+++ sumof2.c
@@ -0,0 +1,19 @@
+//program to find sum of two numbers
+#include <stdio.h> 
+int main()
+{
+ float a;
+float b;
+float sum;
+ 
+ printf("input your first number: \n");
+ scanf ("%f", &a);
+ printf("input your second number: \n ");
+ 
+ 
+ scanf("%f", &b);
+ sum = a + b ;
+ printf("the sum of two numbers is: %f" ,sum);
+ 
+    return 0;
+}
