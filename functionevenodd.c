f3ffcd6c595507e13bf6d06679abb59e40dd56b0
AMO15310 <wachiraamos402@gmail.com>
2022-04-23 12:53:59 -0500

    function to check if a num is even or odd

---
+++ functionevenodd.c
@@ -0,0 +1,16 @@
+//program to a function to check whether a number is even or odd
+#include <stdio.h>
+int evenodd(double) , num;
+int main(){
+  printf("\n Enter a number ");
+  scanf("%d",&num);
+    evenodd(num);
+    return 0;
+}
+int (evenodd(num)){
+if(num%2==0){
+    printf("The number %d is even",num);
+}
+else{
+    printf("The number %d is odd",num);
+}}
